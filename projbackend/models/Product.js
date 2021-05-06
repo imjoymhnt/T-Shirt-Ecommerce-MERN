d3a48b05d729bf3c2c3035978d20b61540ad8830
imjoymhnt <imjoymhnt@pop-os.localdomain>
2021-05-06 22:55:16 +0530

    user, order, category, product model create for backend

---
+++ projbackend/models/Product.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+const {ObjectId} = mongoose.Schema
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 32
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+        maxlength: 2000
+    },
+    price: {
+        type: Number,
+        require: true,
+        trim: true,
+        maxlength: 32
+    },
+    category: {
+        type: ObjectId,
+        ref: "Category",
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true
+    },
+    sold: {
+        type: Number,
+        default: 0
+    },
+    photo: {
+        data: Buffer,
+        contentType: String
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('Product', productSchema)
---
+++ projbackend/models/user.js
@@ -0,0 +1,73 @@
+const mongoose = require('mongoose')
+const crypto = require('crypto')
+const { v4: uuidv4 } = require('uuid');
+
+const Schema = mongoose.Schema()
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: trim,
+        maxlength: 32
+    },
+    lastName: {
+        type: String,
+        maxlength: 32,
+        trim: true        
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    userInfo: {
+        type: String,
+        trim: true
+    },
+    encry_password: {
+        required: true,
+        type: String,
+    },
+    salt: String,
+    role: {
+        type: Number,
+        default: 0
+    },
+    purchases: {
+        type: Array,
+        default: []
+    }
+}, {timestamps: true})
+
+userSchema.virtual('password')
+.set(function(password) {
+    this._password = password
+    this.salt = uuidv4()
+    this.encry_password = this.securePassword(password)
+})
+.get(function() {
+    return this._password
+})
+
+userSchema.method = {
+
+    authenticate: function(plainPassword) {
+        return this.securePassword(plainPassword) === this.encry_password
+    },
+
+    securePassword: function(plainPassword) {
+        if(!password) return "";
+        try {
+            return crypto.createHmac('sha256', this.salt)
+            .update(plainPassword)
+            .digest('hex');
+
+        } catch (err) {
+            return "";
+        }
+    }
+}
+
+module.exports = mongoose.model('User', userSchema)
