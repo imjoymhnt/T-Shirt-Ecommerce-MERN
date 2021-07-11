02e1cc06c815f9af1d2b4041fb7bc03ea427471d
imjoymhnt <mritunjoy.mahanta001@gmail.com>
2021-07-12 00:15:07 +0530

    login base menu item show and adminroute privateroute create

--- projfrontend/src/core/Menu.js
+++ projfrontend/src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
+import { isAuthenticated, signout } from "../auth/helper";
 
 const currentTab = (history, path) => {
   if (history.location.pathname === path) {
@@ -44,33 +45,42 @@ const Menu = ({ history }) => (
           Admin Dashboard
         </Link>
       </li>
-      <li className="nav-item">
-        <Link
-          style={currentTab(history, "/signup")}
-          className="nav-link"
-          to="/signup"
-        >
-          Signup
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          style={currentTab(history, "/signin")}
-          className="nav-link"
-          to="/signin"
-        >
-          Signin
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link
-          style={currentTab(history, "/signout")}
-          className="nav-link"
-          to="/signout"
-        >
-          Signout
-        </Link>
-      </li>
+      {!isAuthenticated() && (
+        <>
+          <li className="nav-item">
+            <Link
+              style={currentTab(history, "/signup")}
+              className="nav-link"
+              to="/signup"
+            >
+              Signup
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              style={currentTab(history, "/signin")}
+              className="nav-link"
+              to="/signin"
+            >
+              Signin
+            </Link>
+          </li>
+        </>
+      )}
+      {isAuthenticated() && (
+        <li className="nav-item">
+          <span
+            className="nav-link text-warning"
+            onClick={() => {
+              signout(() => {
+                history.push("/");
+              });
+            }}
+          >
+            Signout
+          </span>
+        </li>
+      )}
     </ul>
   </div>
 );
--- projfrontend/src/user/Signup.js
+++ projfrontend/src/user/Signup.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { signup } from "../auth/helper";
 import Base from "../core/Base";
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password, error, success } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signup({ name, email, password })
+      .then((data) => {
+        if (data.error !== "undefined") {
+          setValues({ ...values, error: data.error, success: false });
+        } else {
+          setValues({
+            ...values,
+            name: "",
+            email: "",
+            password: "",
+            error: "",
+            success: true,
+          });
+        }
+      })
+      .catch("signup error");
+  };
+
   const signUpForm = () => {
     return (
       <div className="row">
@@ -12,23 +48,72 @@ const Signup = () => {
               <label htmlFor="" className="text-light">
                 Name
               </label>
-              <input className="form-control" type="text" />
+              <input
+                onChange={handleChange("name")}
+                className="form-control"
+                type="text"
+                value={name}
+              />
             </div>
             <div className="form-group">
               <label htmlFor="" className="text-light">
                 Email
               </label>
-              <input className="form-control" type="email" />
+              <input
+                onChange={handleChange("email")}
+                className="form-control"
+                type="email"
+                value={email}
+              />
             </div>
             <div className="form-group">
               <label htmlFor="" className="text-light">
                 Password
               </label>
-              <input className="form-control" type="password" />
+              <input
+                onChange={handleChange("password")}
+                className="form-control"
+                type="password"
+                value={password}
+              />
             </div>
             <br />
-            <button className="btn btn-success btn-block">Submit</button>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              SignUp
+            </button>
           </form>
+        </div>
+      </div>
+    );
+  };
+
+  const successMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: success ? "" : "none" }}
+          >
+            <p>
+              Signup success! Please Login here <Link to="/signin">Login</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
         </div>
       </div>
     );
@@ -36,7 +121,10 @@ const Signup = () => {
 
   return (
     <Base title="signup page" description="A page for user to signUp">
+      {successMessage()}
+      {errorMessage()}
       {signUpForm()}
+      <p className="text-white text-center">{JSON.stringify(values)}</p>
     </Base>
   );
 };
