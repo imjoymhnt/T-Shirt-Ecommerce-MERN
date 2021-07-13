a73b897d866eade792b3ff55114a27666e6f9b25
imjoymhnt <mritunjoy.mahanta001@gmail.com>
2021-07-13 23:35:41 +0530

    final touch on backend

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,103 +1,100 @@
-const User = require('../models/user')
-const { validationResult, cookie } = require('express-validator');
-const user = require('../models/user');
-const jwt = require('jsonwebtoken')
-const expressJwt = require('express-jwt');
-
+const User = require("../models/user");
+const { validationResult, cookie } = require("express-validator");
+const user = require("../models/user");
+const jwt = require("jsonwebtoken");
+const expressJwt = require("express-jwt");
 
 exports.signup = (req, res) => {
+  const errors = validationResult(req);
 
-    const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+      param: errors.array()[0].param,
+    });
+  }
 
-    if(!errors.isEmpty()){
-        return res.status(422).json({
-            error: errors.array()[0].msg,
-            param: errors.array()[0].param
-        })
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "Not able to save user in the DB",
+      });
     }
-
-    const user = new User(req.body)
-    user.save((err, user) => {
-        if(err) {
-            return res.status(400).json({
-                err: "Not able to save user in the DB"
-            })
-        }
-        return res.json({
-            name: user.name,
-            email: user.email,
-            id: user._id
-        })
-    })
-}
+    return res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id,
+    });
+  });
+};
 
 exports.signin = (req, res) => {
-    const {email, password} = req.body
+  const { email, password } = req.body;
 
-    const errors = validationResult(req)
+  const errors = validationResult(req);
 
-    if(!errors.isEmpty()){
-        return res.status(422).json({
-            error: errors.array()[0].msg,
-            param: errors.array()[0].param
-        })
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg,
+      param: errors.array()[0].param,
+    });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: "Email is not valid!",
+      });
+    }
+    if (!user.authenticate(password)) {
+      return res.status(401).json({
+        error: "Email and password don't match!",
+      });
     }
 
-    User.findOne({email}, (err, user) => {
-        if(err || !user) {
-           return res.status(400).json({
-                error: "Email is not valid!"
-            })
-        }
-        if(!user.authenticate(password)) {
-            return res.status(401).json({
-                error: "Email and password don't match!"
-            })
-        }
+    // create token
+    const token = jwt.sign({ _id: user._id }, process.env.SECRET);
 
-        // create token
-        const token = jwt.sign({_id: user._id}, process.env.SECRET)
-        
-        // put token to cookie
-        res.cookie("token", token, {expire: new Date() + 1})
+    // put token to cookie
+    res.cookie("token", token, { expire: new Date() + 1 });
 
-        // send response to frontend
-        const {_id, name, email, role} = user
-        res.json({token, user: {_id, role, email, name}})
-    })
-}
-
+    // send response to frontend
+    const { _id, name, email, role } = user;
+    res.json({ token, user: { _id, role, email, name } });
+  });
+};
 
 exports.signout = (req, res) => {
-    res.clearCookie("token")
-    res.json({
-        message: "User Signout Successfully!"
-    })
-}
+  res.clearCookie("token");
+  res.json({
+    message: "User Signout Successfully!",
+  });
+};
 
 // Protected route
 exports.isSignedIn = expressJwt({
-    secret: process.env.SECRET,
-    userProperty: "auth",
-    algorithms: ['HS256']
-})
+  secret: process.env.SECRET,
+  userProperty: "auth",
+  algorithms: ["HS256"],
+});
 
 // custom middleware
 exports.isAuthenticated = (req, res, next) => {
-    let checker = req.profile && req.auth && req.profile._id == req.auth._id
-    if(!checker) {
-        return res.status(403).json({
-            error: "Access Denied"
-        })
-    }
-    next()
-}
+  let checker = req.profile && req.auth && req.profile._id == req.auth._id;
+  if (!checker) {
+    return res.status(403).json({
+      error: "Access Denied",
+    });
+  }
+  next();
+};
 
 exports.isAdmin = (req, res, next) => {
-    if(req.profile.role === 0) {
-        return res.status(403).json({
-            error: "You are not an Admin, Access Denied!"
-        })
-    }
-    next()
-}
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: "You are not an Admin, Access Denied!",
+    });
+  }
+  next();
+};
